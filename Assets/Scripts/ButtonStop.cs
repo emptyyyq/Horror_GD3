36baa28957e09f0f286b92509f25d719ab434e75
Dima <dmytrobogatyrenko@gmail.com>
2024-12-04 19:56:14 +0200

    Edit background,Add script StopButton, add win scren

---
+++ Assets/Scripts/ButtonStop.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonStop : MonoBehaviour
+{/* КОД НЕ ДІЙСНИЙ!
+    public Button toggleButton;
+    private bool isGamePaused = false;
+
+    void Start()
+    {
+        if (toggleButton != null)
+        {
+            toggleButton.onClick.AddListener(ToggleGame);
+        }
+    }
+
+    public void ToggleGame()
+    {
+        if (isGamePaused)
+        {
+            ResumeGameplay();
+        }
+        else
+        {
+            StopGame();
+        }
+    }
+
+    private void StopGame()
+    {
+        Time.timeScale = 0;
+        isGamePaused = true;
+        Debug.Log("Гру зупинено");
+    }
+
+
+    private void ResumeGameplay()
+    {
+        Time.timeScale = 1;
+        isGamePaused = false;
+        Debug.Log("Гру продовжено");
+    }
+    */
+}
