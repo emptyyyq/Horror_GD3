ff33a3ea73da57434dae2400fe32acde3a21a230
Dima <dmytrobogatyrenko@gmail.com>
2025-03-19 20:28:30 +0200

    flashlight, batteries, 2 buttons, images

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using static UnityEngine.UI.Image;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private Transform ItemHolder;
+     [SerializeField] private Transform ItemHolder;
     [SerializeField] private Animator animator;
     [SerializeField] private float moveSpeed = 100f;
     [SerializeField] private float jumpForce = 5f;
@@ -16,6 +20,27 @@ public class Player : MonoBehaviour
     private bool isGrounded;
 
     private GameObject currentWeapon; // Текущее оружие в руках
+
+    //
+    private Vector2 velocity;
+
+    public Transform playerCamera;
+ 
+
+    private Rigidbody rigidb;
+    public AudioSource myFx;
+    public AudioClip ClipFx;
+
+    public Text txt;
+    public int Energy;
+    public GameObject Light;
+    public bool onLight;
+    private float scet;
+    public GameObject Button_up;
+    private RaycastHit hit;
+    public AudioClip Upfx;
+
+    public List<System.Func<float>> speedOverrides = new List<System.Func<float>>();
 
     private void Start()
     {
@@ -42,6 +67,26 @@ public class Player : MonoBehaviour
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
+
+        txt.text = Energy + "%";
+
+        if (onLight)
+        {
+            scet += Time.deltaTime;
+            if (scet >= 2)
+            {
+                Energy -= 1;
+                scet = 0;
+            }
+        }
+
+        Energy = Mathf.Clamp(Energy, 0, 100);
+
+        if (Energy <= 0)
+        {
+            onLight = false;
+            Light.SetActive(false);
+        }
     }
     public UIHealthBarHelper HealthBarHelper => healthBarHelper;
 
@@ -56,5 +101,51 @@ public class Player : MonoBehaviour
         currentWeapon = Instantiate(weapon, ItemHolder); // Создаем оружие в левой руке
         currentWeapon.transform.localPosition = Vector3.zero;
         currentWeapon.transform.localRotation = Quaternion.identity;
+    } 
+
+  
+    void FixedUpdate()
+    {
+        var forw = transform.TransformDirection(Vector3.forward);
+            Debug.DrawRay(transform.position, forw * 4.5f, Color.red, 2f);
+        if (Physics.Raycast(transform.position, forw, out hit, 4.5f))
+        {
+            Debug.Log(hit.collider.ToString());
+            if (hit.collider.tag == "Battery")
+            {
+                Button_up.SetActive(true);
+            }
+        }
+        else
+        {
+            Button_up.SetActive(false);
+        }
+        
+    }
+    public void FlashLightButton()
+    {
+        myFx.PlayOneShot(ClipFx);
+
+        if (!onLight && Energy > 0)
+        {
+            Light.SetActive(true);
+            onLight = true;
+        }
+        else
+        {
+            Light.SetActive(false);
+            onLight = false;
+        }
+    }
+    public void ButtonUse()
+    {
+        if(hit.collider.tag == "Battery")
+        {
+            Energy += 25;
+            myFx.PlayOneShot(Upfx);
+            Destroy(hit.collider.gameObject);
+        }
     }
 }
+
+
